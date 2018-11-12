55c90a087a8d478776ac191dc55d99a062bca7d4
Dilip Kumar <dilip75074@gmail.com>
2018-11-12 15:39:58 +0530

    Added logic to add contact list into the friends tab.

--- app/src/main/java/com/example/telequiz/FriendsFragment.java
+++ app/src/main/java/com/example/telequiz/FriendsFragment.java
@@ -1,15 +1,38 @@
 package com.example.telequiz;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class FriendsFragment extends Fragment {
 
+    View rootView;
+
+    ListView contactList;
+    Cursor contactCursor;
+    TextView textView;
+
+    String allContacts = "";
     public FriendsFragment() {
         // Required empty public constructor
     }
@@ -22,7 +45,23 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        contactList = rootView.findViewById(R.id.contact_list);
+        textView = rootView.findViewById(R.id.friend_name);
+        contactCursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        getContacts();
+        return rootView;
+    }
+
+    private void getContacts() {
+        while (contactCursor.moveToNext())
+        {
+            String name=contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            allContacts = allContacts  + name + " - " + phoneNumber + "\n";
+        }
+        contactCursor.close();
+        textView.setText(allContacts);
     }
 }
