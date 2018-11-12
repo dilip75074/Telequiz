6d4830218051a4c806514f6fc76119ba76503337
Dilip Kumar <dilip75074@gmail.com>
2018-11-12 10:52:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/telequiz/MainPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.telequiz;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class MainPagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public MainPagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                HomeFragment tab1 = new HomeFragment();
+                return tab1;
+            case 1:
+                InstitutesFragment tab2 = new InstitutesFragment();
+                return tab2;
+            case 2:
+                FriendsFragment tab3 = new FriendsFragment();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
